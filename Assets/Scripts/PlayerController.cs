a477a55e49570bf2b79cc8673d517c7f3a2654de
Sungmoon Choi <158350948+sungpoon04@users.noreply.github.com>
2025-10-09 19:59:06 -0400

    marisa movement + animation done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,33 @@ public class PlayerController : MonoBehaviour
 
     private PlayerInput playerInput;
     private InputAction moveAction;
+    private Rigidbody rb;
 
+    [SerializeField] Transform cam;
     [SerializeField] private float speed = 5;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private Animator animator;
+
+    Vector3 camForward;
+    Vector3 camRight;
+    private Vector3 moveDir;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody>();
+
+        rb.freezeRotation = true;
+
+        camForward = cam.forward;
+        camRight = cam.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
     }
 
     // Update is called once per frame
@@ -24,7 +43,33 @@ public class PlayerController : MonoBehaviour
 
     void MovePlayer()
     {
-        Vector2 direction = moveAction.ReadValue<Vector2>();
-        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+        Vector2 input = moveAction.ReadValue<Vector2>();
+
+        moveDir = camForward * input.y + camRight * input.x;
+        if (input.x != 0 || input.y != 0)
+        {
+            animator.SetFloat("Speed", 1);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
+
+        // Movement
+        if (moveDir.sqrMagnitude > 0.001f)
+        {
+            // Move player
+            transform.position += moveDir * speed * Time.deltaTime;
+
+            // Smoothly rotate toward movement direction
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = rb.position + moveDir * speed * Time.fixedDeltaTime;
+        rb.MovePosition(targetPos);
     }
 }
